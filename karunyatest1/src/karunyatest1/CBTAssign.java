d0bcf39c667b0ca5caa6bf53e0027a695f575c7d
Karunya Damarla <kongari@gmail.com>
2018-11-29 17:39:55 -0600

    Sending 2 assignments

--- karunyatest1/src/karunyatest1/CBTAssign.java
+++ karunyatest1/src/karunyatest1/CBTAssign.java
@@ -5,18 +5,40 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
 
 public class CBTAssign {
 	WebDriver driver;
 
 	@BeforeTest
-
-	public void openbrowser() {
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+	@Parameters("browser")
+	
+	public void openbrowser(String browser)
+	{
+	if(browser.equals("Chrome"))
+	{
+		driver = new ChromeDriver();
+	}
+	else if(browser.equals("FireFox"))
+	{
+		System.setProperty("webdriver.firefox.marionette","geckodriver.exe");
+		driver = new FirefoxDriver();
+	}
+	
+	else if (browser.equals("Internet Explorer"))
+	{
+		System.setProperty("webdriver.ie.driver","IEDriverServer.exe");
+		driver = new FirefoxDriver();
+	}
+		
+		
+		
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
