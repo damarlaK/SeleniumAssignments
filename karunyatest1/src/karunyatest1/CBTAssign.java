5140078664471d102893e7c8670d982e518e0c02
Karunya Damarla <kongari@gmail.com>
2018-11-29 21:49:42 -0600

    New change

--- karunyatest1/src/karunyatest1/CBTAssign.java
+++ karunyatest1/src/karunyatest1/CBTAssign.java
@@ -6,40 +6,33 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-
 public class CBTAssign {
 	WebDriver driver;
 
 	@BeforeTest
 	@Parameters("browser")
-	
-	public void openbrowser(String browser)
-	{
-	if(browser.equals("Chrome"))
-	{
-		driver = new ChromeDriver();
-	}
-	else if(browser.equals("FireFox"))
-	{
-		System.setProperty("webdriver.firefox.marionette","geckodriver.exe");
-		driver = new FirefoxDriver();
-	}
-	
-	else if (browser.equals("Internet Explorer"))
-	{
-		System.setProperty("webdriver.ie.driver","IEDriverServer.exe");
-		driver = new FirefoxDriver();
-	}
+
+	public void openbrowser(String browser) {
+		if (browser.equals("Chrome")) {
+			driver = new ChromeDriver();
+		} 
+		else if (browser.equals("FireFox")) {
+			System.setProperty("webdriver.firefox.marionette", "geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+
+		else if (browser.equals("Internet Explorer")) {
+			System.setProperty("webdriver.ie.driver", "IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
+		}
 		
-		
-		
-		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.get("https://www.linkedin.com//");
